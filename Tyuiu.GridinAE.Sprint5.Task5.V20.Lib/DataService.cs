f6d487b00975b06a493dcbb7c8d089e64c61d2cd
Артём <gridin350@gmail.com>
2024-12-03 11:43:08 +0500

    Task5

--- Tyuiu.GridinAE.Sprint5.Task5.V20.Lib/DataService.cs
+++ Tyuiu.GridinAE.Sprint5.Task5.V20.Lib/DataService.cs
@@ -9,17 +9,16 @@ public class DataService : ISprint5Task5V20
         double count = 0;
         using (var sr = new StreamReader(path))
         {
-            string[] numbers = sr.ReadToEnd().Split();
+            string[] numbers = sr.ReadToEnd().Split(' ');
             count = numbers.Length;
             foreach(string number in numbers)
             {
-                double n = Math.Round(Convert.ToDouble(number.Replace(".", ",")), 3);
-                if (-10 <= n && n <= 10)
+                if (Int32.TryParse(number, out int n) && -10 <= n && n <= 10)
                 {
                     res += n;
                 }       
             }
         }
-        return Math.Round(res / count);
+        return Math.Round(res / count, 3);
     }
 }
